49cc5cc7de53bcc227a3f1fd75864c03bd7189b3
Ahmed <ahmed.max7586@gmail.com>
2019-11-15 02:06:20 -0500

    fixed hamburger menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,98 +1,94 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import TokenService from '../../services/token-service'
-import IdleService from '../../services/idle-service'
-import './Header.css'
+import TokenService from "../../services/token-service";
+import IdleService from "../../services/idle-service";
+import "./Header.css";
 
 export default class Header extends Component {
-
-
   handleLogoutClick = () => {
+    document.getElementById("chk").checked = false;
     TokenService.clearAuthToken();
-        /* when logging out, clear the callbacks to the refresh api and idle auto logout */
-    TokenService.clearCallbackBeforeExpiry()
-    IdleService.unRegisterIdleResets()
-  }
+    /* when logging out, clear the callbacks to the refresh api and idle auto logout */
+    TokenService.clearCallbackBeforeExpiry();
+    IdleService.unRegisterIdleResets();
+  };
+
+  handleUncheck = () => {
+    document.getElementById("chk").checked = false;
+  };
 
   renderLogoutLink() {
     return (
-
-
-        <Link
-          onClick={this.handleLogoutClick}
-          className="btn btn4"
-          to='/'>
-          Logout
-        </Link>
-
-    )
+      <Link
+        onClick={this.handleLogoutClick}
+        
+        className="btn btn4"
+        to="/"
+      >
+        Logout
+      </Link>
+    );
   }
 
   renderLoginLink() {
     return (
-      
-<>
-        <Link
-        className="btn btn4"
-          to='/login'>
+      <>
+        <Link className="btn btn4" onClick={this.handleUncheck} to="/login">
           Log in
         </Link>
 
-          <Link
-          className="btn btn4"
-          to='/register'>
+        <Link className="btn btn4" onClick={this.handleUncheck} to="/register">
           Register
         </Link>
-
       </>
-    )
+    );
   }
 
   render() {
-    return <>
-      <nav className='header'>
-        <h1 className="logo">
-          <Link to='/'>
-            <i className="fas fa-home"></i>Homi
-          </Link>
-        </h1>
+    return (
+      <>
+        <nav className="header">
+          <h1 className="logo">
+            <Link to="/">
+              <i className="fas fa-home"></i>Homi
+            </Link>
+          </h1>
 
-        <input type="checkbox" id="chk"></input>
-        <label htmlFor="chk" className="show-menu-btn">
-          <i className="fas fa-ellipsis-h"></i>
-        </label>
-    
-        <ul className="menu">
-
-        <Link to='/' className="btn btn2">
-        Home
-      </Link>
-          
-          <Link to='/properties' className="btn btn2" >
-          Properties 
-        </Link>
-        <Link to='/listMyPropertyPage' className="btn btn2" >
-        List My Property
-      </Link>
-
-
-          
-          {TokenService.hasAuthToken()
-            ? this.renderLogoutLink()
-            : this.renderLoginLink()}
-          <label htmlFor="chk" className="hide-menu-btn">
-            <i className="fas fa-times"></i>
+          <input type="checkbox" id="chk"></input>
+          <label htmlFor="chk" className="show-menu-btn">
+            <i className="fas fa-ellipsis-h"></i>
           </label>
-        </ul>
 
+          <ul className="menu">
+            <Link to="/" onClick={this.handleUncheck} className="btn btn2">
+              Home
+            </Link>
 
+            <Link
+              to="/properties"
+              onClick={this.handleUncheck}
+              className="btn btn2"
+            >
+              Properties
+            </Link>
+            <Link
+              to="/listMyPropertyPage"
+              onClick={this.handleUncheck}
+              className="btn btn2"
+            >
+              List My Property
+            </Link>
 
-
-          
-      </nav>
-
-
-    </>
+            {TokenService.hasAuthToken()
+              ? this.renderLogoutLink()
+              : this.renderLoginLink()}
+            <label htmlFor="chk" className="hide-menu-btn">
+              <i className="fas fa-times"></i>
+            </label>
+          </ul>
+        </nav>
+      </>
+    );
   }
 }
